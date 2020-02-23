7e782621e23f2298fe94052d1e5b40d2bd733309
Tsuno Yuya <2ch.yuya@gmail.com>
2020-02-23 21:44:04 +0900

    バリデーションの追加、テスト

--- db/migrate/20200202095657_create_tasks.rb
+++ db/migrate/20200202095657_create_tasks.rb
@@ -1,12 +1,13 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :title
-      t.text :content
+      t.string :title, null: false
+      t.text :content, null: false
       t.date :limit
       t.integer :priority
       t.integer :status
       t.integer :user_id
+      #TODO 後ほど新しいmigrationファイルでuser_idにnull: falseを追加します。
 
       t.timestamps
     end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,18 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
 
   it 'titleが空ならバリデーションが通らない' do
     task = Task.new(title: '', content: '失敗テスト')
     expect(task).not_to be_valid
   end
+
   it 'contentが空ならバリデーションが通らない' do
     task = Task.new(title: '失敗テスト', content: '')
+    # binding.pry
     expect(task).not_to be_valid
   end
+
   it 'titleとcontentに内容が記載されていればバリデーションが通る' do
-    task = Task.new(title: '成功テスト', content: '成功テスト')
-    expect(task).not_to be_valid
+    task = Task.create(title: '成功テスト', content: '成功テスト')
+    # binding.irb
+    expect(task).to be_valid
   end
 end
