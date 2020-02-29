a99a9dd0045c58932131a9de17cece2827d028b7
Tsuno Yuya <2ch.yuya@gmail.com>
2020-02-29 16:43:03 +0900

    ?

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   #TODO 後ほどuser_idにpresence: trueを追加します。
+
+  def search
+    @tasks = Task.where("title LIKE ?", "%#{ params[:task][:title] }%")
+  end
 end
