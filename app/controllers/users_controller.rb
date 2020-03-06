0384e5e1dc8380c300913812de387522ecee67da
Tsuno Yuya <2ch.yuya@gmail.com>
2020-03-06 15:55:45 +0900

    ユーザ登録（create）をした時、同時にログイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,10 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        session[:user_id] = @user.id
+        format.html { redirect_to @user, notice: 'User was successfully created and logged in.' }
+        format.json { render :show, status: :created, locexitation: @user }
+        # binding.pry
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
