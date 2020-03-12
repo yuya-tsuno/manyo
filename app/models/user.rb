7048549f7341d1a3a11cd0b96b42406becfe19b7
Tsuno Yuya <2ch.yuya@gmail.com>
2020-03-12 17:17:01 +0900

    userモデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   before_validation { email.downcase! }
   
+  validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 4 }, on: :create
   
