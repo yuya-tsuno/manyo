7c7e66b60012051f6946e8b75b3df034c8349436
Tsuno Yuya <2ch.yuya@gmail.com>
2020-03-14 18:54:21 +0900

    localのエラーページ変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
+  # falseで一般ユーザー用のエラーページに変更します。serverの再起動忘れずに。
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
